31684fb336bdc258a236bf2fb8ca7bfec89e3bea
Ronald Paloschi <ronald.paloschi@gmail.com>
2017-04-02 17:39:12 -0300

    Line Types and unit tests.

---
+++ sections/linetype.go
@@ -0,0 +1,100 @@
+package sections
+
+import (
+	"fmt"
+	"github.com/rpaloschi/dxf-go/core"
+)
+
+const absRotationBit = 0x1
+const textStringBit = 0x2
+const elementShapeBit = 0x4
+
+type LineElement struct {
+	Length           float64
+	AbsoluteRotation bool
+	IsTextString     bool
+	IsShape          bool
+	ShapeNumber      int
+	Scale            float64
+	RotationAngle    float64
+	XOffset          float64
+	YOffset          float64
+	Text             string
+}
+
+type LineType struct {
+	Name        string
+	Description string
+	Length      float64
+	Pattern     []*LineElement
+}
+
+func NewLineType(tags core.TagSlice) *LineType {
+	ltype := new(LineType)
+	ltype.Pattern = make([]*LineElement, 0)
+
+	flags74 := 0
+	var lineElement *LineElement
+	for _, tag := range tags.RegularTags() {
+		switch tag.Code {
+		case 2:
+			ltype.Name, _ = core.AsString(tag.Value)
+
+		case 3:
+			ltype.Description, _ = core.AsString(tag.Value)
+
+		case 40:
+			ltype.Length, _ = core.AsFloat(tag.Value)
+
+		case 49:
+			if lineElement != nil {
+				ltype.Pattern = append(ltype.Pattern, lineElement)
+			}
+			lineElement = new(LineElement)
+			lineElement.Scale = 1.0
+			lineElement.Length, _ = core.AsFloat(tag.Value)
+
+		case 74:
+			flags74, _ = core.AsInt(tag.Value)
+			if flags74 > 0 {
+				lineElement.AbsoluteRotation = flags74&absRotationBit > 0
+				lineElement.IsTextString = flags74&textStringBit > 0
+				lineElement.IsShape = flags74&elementShapeBit > 0
+			}
+
+		case 75:
+			flags, _ := core.AsInt(tag.Value)
+			if flags74 == 0 {
+				fmt.Print("WARNING! there should be no 75 Code tag if 74 value is 0\n")
+			} else if lineElement.IsTextString && flags != 0 {
+				fmt.Print("WARNING! Tag 75 should be 0 if 74 is a TextString\n")
+			} else if lineElement.IsShape {
+				lineElement.ShapeNumber = flags
+			}
+
+		case 46:
+			lineElement.Scale, _ = core.AsFloat(tag.Value)
+
+		case 50:
+			lineElement.RotationAngle, _ = core.AsFloat(tag.Value)
+
+		case 44:
+			lineElement.XOffset, _ = core.AsFloat(tag.Value)
+
+		case 45:
+			lineElement.YOffset, _ = core.AsFloat(tag.Value)
+
+		case 9:
+			lineElement.Text, _ = core.AsString(tag.Value)
+
+		default:
+			fmt.Printf("Discarding tag for Style: %+v\n", tag.ToString())
+		}
+	}
+
+	if lineElement != nil {
+		ltype.Pattern = append(ltype.Pattern, lineElement)
+	}
+
+	return ltype
+}
---
+++ sections/linetype_test.go
@@ -0,0 +1,88 @@
+package sections
+
+import (
+	"github.com/rpaloschi/dxf-go/core"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func lineTypeFromDxfFragment(fragment string) *LineType {
+	next := core.Tagger(strings.NewReader(fragment))
+	tags := core.TagSlice(core.AllTags(next))
+	return NewLineType(tags)
+}
+
+func TestLineTypeDefaultValues(t *testing.T) {
+	lineType := lineTypeFromDxfFragment("")
+
+	assert.Equal(t, "", lineType.Name)
+	assert.Equal(t, "", lineType.Description)
+	assert.InDelta(t, 0.0, lineType.Length, 0.001)
+	assert.Equal(t, []*LineElement{}, lineType.Pattern)
+}
+
+const dxfLineType = ` 2
+DASHDOT
+  3
+Strange dashdot
+ 72
+    65
+ 73
+     4
+ 40
+1.0
+ 49
+0.4
+ 74
+3
+ 46
+1.0
+ 50
+0.5
+ 44
+1.5
+ 45
+2.3
+  9
+Sample Text
+ 49
+0.3
+ 74
+5
+ 75
+1
+ 46
+1.5
+ 50
+1.0
+ 44
+0.1
+ 45
+0.2
+ 49
+0.3
+ 74
+4
+ 75
+2
+`
+
+func TestParseLineType(t *testing.T) {
+	lineType := lineTypeFromDxfFragment(dxfLineType)
+
+	assert.Equal(t, "DASHDOT", lineType.Name)
+	assert.Equal(t, "Strange dashdot", lineType.Description)
+	assert.InDelta(t, 1.0, lineType.Length, 0.001)
+
+	expectedElements := []*LineElement{
+		{0.4, true, true, false, 0,
+			1.0, 0.5, 1.5, 2.3, "Sample Text"},
+		{0.3, true, false, true, 1,
+			1.5, 1.0, 0.1, 0.2, ""},
+		{0.3, false, false, true, 2,
+			1.0, 0.0, 0.0, 0.0, ""},
+	}
+
+	assert.Equal(t, expectedElements, lineType.Pattern)
+}
