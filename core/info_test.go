dc17257d0abab0afbdf66d03385d6fa3e067b316
Ronald Paloschi <ronald.paloschi@gmail.com>
2017-04-09 22:32:46 -0300

    DXF info unit tests

--- core/info_test.go
+++ core/info_test.go
@@ -30,9 +30,59 @@ EOF
 `
 
 func TestDXFInfo(t *testing.T) {
-	info, _ := GetDXFInfo(strings.NewReader(dxfWithInfo))
+	info, err := GetDXFInfo(strings.NewReader(dxfWithInfo))
+
+	assert.Nil(t, err)
 	assert.Equal(t, "R2004", info.Release)
 	assert.Equal(t, "AC1018", info.Version)
 	assert.Equal(t, "cp1252", info.Encoding)
 	assert.Equal(t, "1", info.Handseed)
+}
+
+func TestDXFInfoError(t *testing.T) {
+	info, err := GetDXFInfo(strings.NewReader("  XXX\nSECTION"))
+
+	assert.Equal(t, Info{}, info)
+	assert.NotNil(t, err)
+}
+
+const dxfInfoAttributeError = `  0
+SECTION
+  2
+HEADER
+  9
+$ACADVER
+  XXX
+AC1018
+  0
+ENDSEC
+  0
+EOF
+`
+
+func TestDXFInfoAttributeError(t *testing.T) {
+	info, err := GetDXFInfo(strings.NewReader(dxfInfoAttributeError))
+
+	assert.Equal(t, Info{}, info)
+	assert.NotNil(t, err)
+}
+
+const dxfInfoDefaultACADVER = `  0
+SECTION
+  2
+HEADER
+  9
+$ACADVER
+  1
+XXX
+  0
+ENDSEC
+  0
+EOF
+`
+
+func TestDXFInfoDefaultAcadVer(t *testing.T) {
+	info, _ := GetDXFInfo(strings.NewReader(dxfInfoDefaultACADVER))
+
+	assert.Equal(t, "R12", info.Release)
 }
